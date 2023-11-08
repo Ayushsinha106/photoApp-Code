c06608d56a65b38d79dd9ebe5649b81ffb637a16
Aayush Sinha <121587024+Ayushsinha106@users.noreply.github.com>
2023-11-08 19:54:17 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/compat/storage';
 
 
   const firebaseConfig = {
-    apiKey: "AIzaSyA-fo648HeZTqDOR4eKzI0nOCk2JdCMxmg",
-    authDomain: "photoapp-f9e50.firebaseapp.com",
-    projectId: "photoapp-f9e50",
-    storageBucket: "photoapp-f9e50.appspot.com",
-    messagingSenderId: "16404813897",
-    appId: "1:16404813897:web:1349b46969b79ac4660798"
+    //YOUR FIREBASE CONFIGURATIONS
   };
 
   firebase.initializeApp(firebaseConfig);
